6a1dd89fe05a90ff2b98dd3385fe06a075e6f0a0
Luis Fernando <fernando.osousa@alu.ufc.br>
2024-04-02 09:26:13 -0300

    delete: arquivo de teste de funcionalidades

--- Teste2.py
+++
@@ -1,4 +0,0 @@
-with open('./files/plan-traces-teste.txt', 'r') as arquivo:
-    for linha in arquivo:
-        valores_numericos = [int(valor) for valor in linha.strip().split(':')]
-        print(valores_numericos)
